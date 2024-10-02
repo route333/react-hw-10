4e4ffd4bcb755cec99b9e363c3637bc5159bfbdd
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2024-10-02 19:34:14 +0200

    hw 12

--- hw10/src/Components/TelephoneBook/PhoneBook.jsx
+++ hw10/src/Components/TelephoneBook/PhoneBook.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ContactForm from "./ContactForm";
-import Filter from "./Filter";
+import React, { Component } from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
 
-class PhoneBook extends React.Component {
+class PhoneBook extends Component {
   state = {
-    contacts: [
+    contacts: JSON.parse(localStorage.getItem('contacts')) || [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
@@ -12,15 +12,26 @@ class PhoneBook extends React.Component {
     ],
   };
 
+  componentDidMount() {
+    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storedContacts) {
+      this.setState({ contacts: [...this.state.contacts, ...storedContacts] });
+    }
+  }
+
   addContact = (newContact) => {
     this.setState({
       contacts: [...this.state.contacts, newContact],
+    }, () => {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     });
   };
 
   deleteContact = (id) => {
     this.setState({
       contacts: this.state.contacts.filter((contact) => contact.id !== id),
+    }, () => {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     });
   };
 
@@ -30,14 +41,14 @@ class PhoneBook extends React.Component {
         <h1>Phonebook</h1>
         <ContactForm
           onAddContact={this.addContact}
-          onDeleteContact={this.deleteContact} 
+          onDeleteContact={this.deleteContact}
           contacts={this.state.contacts}
         />
 
         <h2>Contacts</h2>
         <Filter
           contacts={this.state.contacts}
-          onDeleteContact={this.deleteContact} 
+          onDeleteContact={this.deleteContact}
         />
       </div>
     );
